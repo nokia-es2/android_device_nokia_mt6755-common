27d7077529800f9dd086edcc69fd8cf90e7bf68f
adomerlee <adomerlee@gmail.com>
2022-08-07 07:46:29 +0400

    mt6755-common: Initial skeleton

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/nokia/mt6755-common
+
+# Inherit the proprietary files
+include vendor/nokia/mt6755-common/BoardConfigVendor.mk
---
+++ mt6755.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/nokia/mt6755-common/mt6755-common-vendor.mk)
