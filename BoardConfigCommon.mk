2218aacea50a9b91f833b004b1197adc38bb09bc
adomerlee <adomerlee@gmail.com>
2022-08-07 07:50:45 +0400

    mt6755-common: Add arch and bootloader flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,5 +6,24 @@
 
 COMMON_PATH := device/nokia/mt6755-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := generic
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6750
+TARGET_NO_BOOTLOADER := true
+
 # Inherit the proprietary files
 include vendor/nokia/mt6755-common/BoardConfigVendor.mk
