19f749a11c164d43f729cab44cd7157ff8dd8a15
adomerlee <adomerlee@gmail.com>
2022-08-11 09:19:29 +0400

    mt6755-common: Import compatibility matrix and manifest

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,5 +72,10 @@ BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 # Treble
 BOARD_VNDK_VERSION := 29
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
+
 # Inherit the proprietary files
 include vendor/nokia/mt6755-common/BoardConfigVendor.mk
