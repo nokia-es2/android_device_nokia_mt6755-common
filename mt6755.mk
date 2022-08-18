5e808d366b8b261d6ebb79f557a91e8f4af37326
adomerlee <adomerlee@gmail.com>
2022-08-18 05:06:30 +0400

    mt6755-common: Build atrace service

--- mt6755.mk
+++ mt6755.mk
@@ -7,6 +7,11 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
