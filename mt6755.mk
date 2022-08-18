e8302f368cb2efbc854cd791dfa5306e43e31c83
adomerlee <adomerlee@gmail.com>
2022-08-18 04:16:55 +0400

    mt6755-common: Setup namespace

--- mt6755.mk
+++ mt6755.mk
@@ -7,5 +7,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/nokia/mt6755-common/mt6755-common-vendor.mk)
